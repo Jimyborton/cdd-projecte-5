3f43dbe43277457c33b265b7e74c5553a91a5fa4
José García de las Bayonas Martinez <86870579+Jimyborton@users.noreply.github.com>
2024-04-29 17:17:53 +0200

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.temperature() < 28) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+})
